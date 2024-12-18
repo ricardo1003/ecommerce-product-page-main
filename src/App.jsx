4eb3cf3d8de9cc944c80e85435bb4faf35739cb9
unknown <ricardodonado1105@gmail.com>
2024-12-18 13:34:17 -0500

    mobile navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -4,18 +4,41 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+  const [navState, setNavState] = useState(false);
+
   return (
     <>
-      <header className="flex gap-4 w-full p-6 items-center justify-between">
-        <nav>
-          <button className="flex items-center">
+      <header className="relative flex gap-4 w-full p-6 pl-[56px] items-center justify-between">
+          <button
+            className="flex items-center absolute left-6 z-[3]"
+            onClick={() => {
+              navState ? setNavState(false) : setNavState(true);
+            }}
+          >
             <picture>
-              <img src="./assets/icon-menu.svg" alt="menu" />
-              <img src="./assets/icon-close.svg" alt="close" className="hidden"/>
+              <img
+                src="./assets/icon-menu.svg"
+                alt="menu"
+                className={navState ? "hidden" : "block"}
+              />
+              <img
+                src="./assets/icon-close.svg"
+                alt="close"
+                className={navState ? "block" : "hidden"}
+              />
             </picture>
           </button>
+        <nav className={`fixed left-0 font-bold bg-white top-0 p-6 pt-24 w-[225px] z-[2] h-[100vh] ${navState ? "translate-x-[0]" : "-translate-x-[225px]"} transition-transform duration-500`}>
+          <ul className="flex flex-col gap-3">
+            <li><a href="">Collections</a></li>
+            <li><a href="">Men</a></li>
+            <li><a href="">Women</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Contact</a></li>
+          </ul>
         </nav>
-        <img src="./assets/logo.svg" alt="logo" className="mr-auto"/>
+        <div className={`fixed block w-full h-full bg-black/50 top-0 left-0 z-[1] opacity-0 ${navState ? "block opacity-100" : "opacity-0"} transition-all duration-500`}></div>
+        <img src="./assets/logo.svg" alt="logo" className="mr-auto" />
         <a href="">
           <img src="./assets/icon-cart.svg" alt="cart" />
         </a>
