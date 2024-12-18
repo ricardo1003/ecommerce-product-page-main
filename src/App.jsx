834bda4bc108695a0486e23d25b54efe96a7ebc6
unknown <ricardodonado1105@gmail.com>
2024-12-18 16:05:44 -0500

    info section done with working counter

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,9 @@ import "./App.css";
 function App() {
   const [navState, setNavState] = useState(false);
 
-  const [imgNumber, setImgNumber] = useState(0)
+  const [imgNumber, setImgNumber] = useState(0);
+
+  const [counter, setCounter] = useState(0);
 
   return (
     <>
@@ -68,21 +70,102 @@ function App() {
         </a>
       </header>
       <main className="max-w-[100vw]">
-        <div className="flex justify-between items-center w-full min-h-[375.2px] relative overflow-hidden" >
-          <button type="button" className={`block bg-white size-10 z-[5] mx-4 rounded-full ${(imgNumber <= 0) ? "bg-white/25" : ""}`} onClick={()=>{setImgNumber(imgNumber - 1)}} disabled={(imgNumber <= 0) ? true : false}>
-            <img src="./assets/icon-previous.svg" alt="previous" className="mx-3" />
+        <div className="flex justify-between items-center w-full min-h-[375.2px] relative overflow-hidden">
+          <button
+            type="button"
+            className={`block bg-white size-10 z-[5] mx-4 rounded-full ${
+              imgNumber <= 0 ? "bg-white/25" : ""
+            }`}
+            onClick={() => {
+              setImgNumber(imgNumber - 1);
+            }}
+            disabled={imgNumber <= 0 ? true : false}
+          >
+            <img
+              src="./assets/icon-previous.svg"
+              alt="previous"
+              className="mx-3"
+            />
           </button>
-          <picture className={`absolute flex z-[1]  `} style={{translate: `${-100 * imgNumber}vw`, transitionDuration:"250ms"}}>
+          <picture
+            className={`absolute flex z-[1]  `}
+            style={{
+              translate: `${-100 * imgNumber}vw`,
+              transitionDuration: "250ms",
+            }}
+          >
             <img src="./assets/image-product-1.jpg" alt="image-1" />
             <img src="./assets/image-product-2.jpg" alt="image-2" />
             <img src="./assets/image-product-3.jpg" alt="image-3" />
             <img src="./assets/image-product-4.jpg" alt="image-4" />
           </picture>
-          <button type="button" className={`block bg-white size-10 z-[5] mx-4 rounded-full ${(imgNumber >= 3) ? "bg-white/25" : ""}`} onClick={()=>{setImgNumber(imgNumber + 1)}} disabled={(imgNumber >= 3) ? true : false}>
+          <button
+            type="button"
+            className={`block bg-white size-10 z-[5] mx-4 rounded-full ${
+              imgNumber >= 3 ? "bg-white/25" : ""
+            }`}
+            onClick={() => {
+              setImgNumber(imgNumber + 1);
+            }}
+            disabled={imgNumber >= 3 ? true : false}
+          >
             <img src="./assets/icon-next.svg" alt="next" className="mx-4" />
           </button>
         </div>
-        <section></section>
+        <section className="p-4">
+          <h3 className="uppercase font-bold text-Dark-grayish-blue">
+            sneaker company
+          </h3>
+          <h2 className="capitalize font-bold text-3xl">
+            fall limited edition sneakers
+          </h2>
+          <p className="text-Dark-grayish-blue my-4">
+            These low-profile sneakers are your perfect casual wear companion.
+            Featuring a durable rubber outer sole, they’ll withstand everything
+            the weather can offer.
+          </p>
+          <h4 className="text-3xl font-bold w-full flex gap-2">
+            $125.00{" "}
+            <span className="text-white bg-black rounded-lg text-xl px-2 py-1 mr-auto">
+              50%
+            </span>{" "}
+            <span className="text-xl text-Dark-grayish-blue line-through">
+              $250.00
+            </span>
+          </h4>
+          <form action="" className="w-full flex flex-col gap-2 my-8">
+            <div className="flex justify-between items-center font-bold bg-Light-grayish-blue h-12 rounded-lg">
+              <button
+                type="button"
+                className="px-8 h-full"
+                onClick={(e) => {
+                  e.preventDefault, setCounter(counter - 1);
+                }}
+                disabled={counter ? false : true}
+              >
+                <img src="./assets/icon-minus.svg" alt="minus" />
+              </button>
+              {counter}
+              <button
+                type="button"
+                className="px-8 h-full"
+                onClick={(e) => {
+                  e.preventDefault, setCounter(counter + 1);
+                }}
+              >
+                <img src="./assets/icon-plus.svg" alt="plus" />
+              </button>
+            </div>
+            <button className="bg-Orange h-12 rounded-lg flex items-center justify-center gap-4 font-bold">
+              <img
+                src="./assets/icon-cart.svg"
+                alt="cart"
+                className="brightness-0"
+              />
+              Add to cart
+            </button>
+          </form>
+        </section>
       </main>
     </>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}"
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     fontFamily: {
-      Kumbh: ["Kumbh Sans"]
+      Kumbh: ["Kumbh Sans"],
     },
-    extend: {},
+    extend: {
+      colors: {
+        "Very-dark-blue": "hsl(220, 13%, 13%)",
+        "Dark-grayish-blue": "hsl(219, 9%, 45%)",
+        "Grayish-blue": "hsl(220, 14%, 75%)",
+        "Light-grayish-blue": "hsl(223, 64%, 98%)",
+        "Orange": "hsl(26, 100%, 55%)",
+        "Pale orange": "hsl(25, 100%, 94%)",
+      },
+    },
   },
   plugins: [],
-}
-
+};
