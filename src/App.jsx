fee47ef423cf9ee3ea9576ee14d150d497bce419
unknown <ricardodonado1105@gmail.com>
2024-12-18 12:59:44 -0500

    mobile header created (without working nav)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
-import { useState } from 'react'
-import reactLogo from '/assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "/assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <h1 className='font-Kumbh'>hello world!!!</h1>
+      <header className="flex gap-4 w-full p-6 items-center justify-between">
+        <nav>
+          <button className="flex items-center">
+            <picture>
+              <img src="./assets/icon-menu.svg" alt="menu" />
+              <img src="./assets/icon-close.svg" alt="close" className="hidden"/>
+            </picture>
+          </button>
+        </nav>
+        <img src="./assets/logo.svg" alt="logo" className="mr-auto"/>
+        <a href="">
+          <img src="./assets/icon-cart.svg" alt="cart" />
+        </a>
+        <a href="" className="max-w-8">
+          <img src="./assets/image-avatar.png" alt="user" />
+        </a>
+      </header>
+      <main></main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
